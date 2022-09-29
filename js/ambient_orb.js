9a581621788a82a4f498c44f8fad3128d6d43ee5
Benjamin Lucas <benjaminlucas@benjamins-mbp.mynetworksettings.com>
2022-09-29 14:50:42 -0400

    add disco ball

--- js/ambient_orb.js
+++ js/ambient_orb.js
@@ -8,14 +8,98 @@ document.body.appendChild(renderer.domElement);
 var orbital_radius = 5;
 var orb_radius = 2.2;
 
-var geometry = new THREE.SphereGeometry(orb_radius, 128, 128);
 
-const texture = new THREE.TextureLoader().load('Assets/cloudtex.jpeg' );
+function vertexShader() {
+  return `
+  uniform float amplitude;
 
-var material = new THREE.MeshBasicMaterial( { map: texture } );
+  attribute float displacement;
+
+  varying vec3 vNormal;
+  varying vec3 vPosition;
+  varying vec2 vUv;
+
+  void main() {
+
+    vNormal = normal;
+    vPosition = position;
+    
+      vec3 offset = position;
+      offset.xyz += normal * amplitude;
+      
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(offset,1);
+  }
+      `
+}
+
+function fragmentShader() {
+  return `
+  varying vec3 vNormal;
+  varying vec3 vPosition;
+  varying vec2 vUv;
+
+  uniform vec3 color;
+  uniform sampler2D colorTexture;
+  uniform vec3 fadePoint;
+  uniform float fadeIntensity;
+
+  void main() {
+
+    vec3 light = vec3( 0.5, 0.2, 1.0 );
+    light = normalize( light );
+
+    float dProd = dot( vNormal, light ) * 0.5 + 0.5;
+
+    vec4 tcolor = texture2D( colorTexture, vUv );
+    vec4 gray = vec4( vec3( tcolor.r * 0.3 + tcolor.g * 0.59 + tcolor.b * 0.11 ), 1.0 );
+
+    
+    float x = dot( vPosition, fadePoint );
+    vec4 popFade;
+    if(x > fadeIntensity)
+    {
+      popFade = vec4(1,1,1,1);
+    }
+    else{
+      popFade = vec4(1,1,1,1);
+    }
+
+
+    gl_FragColor = popFade * gray * vec4( vec3( dProd ) * vec3( color ), 1.0 );
+  }
+  `
+}
+
+
+function createMaterials() {      
+  const cubeShader = new THREE.ShaderMaterial({
+    uniforms: {
+      amplitude: {type: 'float', value: 0.2},
+        color: {type: 'vec3', value: new THREE.Color(0xfff)},
+        fadePoint: {type: 'vec3', value: new THREE.Vector3(1,0,0)},
+        fadeIntensity: {type: 'float', value: 0.3},
+        colorTexture: { value: new THREE.TextureLoader().load( 'Assets/cloudtex.jpeg' ) }
+    },
+    vertexShader: vertexShader(),
+    fragmentShader: fragmentShader()
+  });
+
+  return {
+      cubeShader
+  }
+}
+
+//var geometry = new THREE.SphereGeometry(orb_radius, 128, 128);
+const width = 1;
+const height = 1;
+const depth = 1;
+const geometry = new THREE.BoxBufferGeometry(width, height, depth);const texture = new THREE.TextureLoader().load('Assets/cloudtex.jpeg' );
+
+//var material = new THREE.MeshBasicMaterial( { map: texture } );
 //var material = new THREE.MeshLambertMaterial();
+var material = createMaterials();
 
-var orb = new THREE.Mesh(geometry, material);
+var orb = new THREE.Mesh(geometry, material.cubeShader);
 orb.scale.set(1,1.4,1);
 scene.add(orb);
 
@@ -47,8 +131,13 @@ RotateCamera(0);
 function Update()
 {
   renderer.render(scene, camera);
-  orb.position.set(Math.sin(increment), 0, 0)
-  increment += 0.1;
+  //orb.position.set(Math.sin(increment), 0, 0)
+  increment += 0.01;
+  //orb.rotation.set(0,increment,0);
+  orb.material.uniforms.amplitude = {type: "float", value:  0.5*(1 + Math.sin(increment))}
+  //orb.material.uniforms.fadeIntensity = {type: "float", value:  Math.sin(increment)}
+  //orb.material.uniforms.fadePoint = {type: 'vec3', value: new THREE.Vector3(1 + Math.sin(increment),0,0) },
+
   requestAnimationFrame(Update);
 }
 Update();
